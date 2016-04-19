72a158923694d60b2ebe800817d75fba9c34a71a
DouglasShiro <douglasshiro@hotmail.com>
2016-04-19 02:38:20 -0300

    19/04/2016
    Organização arquivos, criação arquivos tasks_montador, tasks_ligador(alterações a serem feitas durante trabalho), ligador.c,
    montador.c(necessário criar novos arquivos para modularizacao do programa), reference.txt(arquivo com tabelas de instrucoes
    e diretivas), pasta Docs, contendo o .pdf da descricao do trabalho e um ReadMe com o nome dos componentes e Objetivo do trabalho.

---
+++ ligador.c
@@ -0,0 +1,13 @@
+/* PROGRAMA LIGADOR
+* <COMANDO> ./ligador.c <entrada_1.o entrada_2.o >saida.e
+*
+*   Grupo:  Andressa Valadares          13/0042943
+*           Douglas Shiro Yokoyama      13/0024902
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    return 0;
+}
---
+++ montador.c
@@ -0,0 +1,16 @@
+/* PROGRAMA MONTADOR
+*  <COMANDO> ./montador.c <entrada.asm > saida.o 
+*
+*   Grupo:  Andressa Valadares          13/0042943
+*           Douglas Shiro Yokoyama      13/0024902
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    
+    
+    return 0;
+}
