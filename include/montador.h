3daeca009c39103e8990f3b60a314936aef0649a
Andressa Valadares <andressarodrial@hotmail.com>
2016-05-01 21:16:16 -0300

    Função pre_processor: pronta, verifica erros de tamanho. Funcao tokens_validos: verifica se os caracteres correspondem ao esperado senao exibe ERRO LEXICO - linha - token

--- include/montador.h
+++ include/montador.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include "tabelas.h"
 #define TOK_SIZE  100
-
-
-
-
-
 
 typedef struct TOKENS {
 	int	line; 	/* Linha do programa que o token esta*/
@@ -16,11 +9,16 @@ typedef struct TOKENS {
 	struct TOKENS*	next;  	/* Proximo token */
 }Tokens;
 
-
-Tokens* pre_processor(char* nameFile, Tokens* tableTok);
+/* Funcoes de Tokens */
 Tokens* insert_token(Tokens* tableTok, char* token, int line, char type);
 Tokens* inicialize_token(void);
 void print_tokens(Tokens* tableTok);
 Tokens* remove_token(Tokens* tableTok);
+
+/* Funcoes do montador*/
+Tokens* pre_processor(char* nameFile, Tokens* tableTok);
+//int primeira_passagem(Tokens* tableTok, Tab_lst* tableDef);
+int primeira_passagem(Tokens* tableTok, Tab_lst* tableSimb, Tab_lst* tableDir, Tab_lst* tableDef, Tab_lst* tableUso);
+int token_valido(char* token, int nLine);
 
 
