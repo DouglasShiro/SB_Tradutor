e603831cc4fd9c0e1c45968c07be1102042936b1
douglasshiro <douglasshiro@hotmail.com>
2016-05-03 01:28:07 -0300

    Parte: ligador, inserçao de comentarios

--- src/ligador.c
+++ src/ligador.c
@@ -1,33 +1,15 @@
 /* PROGRAMA LIGADOR
-* <COMANDO> ./ligador.c <entrada_1.o entrada_2.o >saida.e
+* <COMANDO> ./ligador.c entrada_1.o entrada_2.o saida.e
 *
 *   Grupo:  Andressa Valadares          13/0042943
 *           Douglas Shiro Yokoyama      13/0024902
-*/
-
-/*
-TASK -
-ler tabela uso OK
-ler tabela definicao OK
-gerar tabela de fatores de correcao OK
-criar tabela global de definicoes OK
-
-1.− Alinha os codigos objeto a serem ligados OK
-2.− Construir tabela com fatores de correcao OK
-3.− Construir tabela global de definicoes, utilizando os fatores de correcao. OK
-4.− Para cada objeto:
-4.1.− Corrigir os enderecos das entradas da tabela de uso, utilizando a tabela global de definicoes
-4.2.− Corrigir os enderecos do codigo usando os fatores de correcao
-4.3.− Corrigir os enderecos relativos usando os fatores de correcao
-5.− Gerar codigo executavel e salvar em arquivo
 */
 
 #include "ligador.h"
 
 int main(int argc, char** argv){
 
-    //int fatorCorrecao[2];
-    int fatCorAux = 0;
+    int fatorCorrecao = 0;
 
     char* nomeArquivo;
 
@@ -51,22 +33,22 @@ int main(int argc, char** argv){
         nomeArquivo = argv[3];
         out = fopen(nomeArquivo, "w");
 
-        /*verifica erro na abertura do arquivo*/
+        /*verifica erro ao abrir arquivos*/
         if((modA != NULL) && (modB != NULL) && (out != NULL)){
 
             /*Leitura do codigo objeto dos modulos A e B
                 - geracao tabelas de uso e definicao
                 - leitura enderecos relativos
-                - uniao codigos objetos modA e modB
-            */
-            //fatorCorrecao[0] = 0;
-            ler_objeto(modA, &TabUsoA, &TabDefA, &TabGlobalDef, &fatCorAux, 0);
-            //fatorCorrecao[1] = fatCorAux;
-            ler_objeto(modB, &TabUsoB, &TabDefB, &TabGlobalDef, &fatCorAux, 1);
+                - uniao codigos objetos modA e modB*/
 
+            ler_objeto(modA, &TabUsoA, &TabDefA, &TabGlobalDef, &fatorCorrecao, 0);
+            ler_objeto(modB, &TabUsoB, &TabDefB, &TabGlobalDef, &fatorCorrecao, 1);
+
+            /*Retornando ao inicio do arquivo*/
             rewind(modA);
             rewind(modB);
 
+            /*Corrige e grava o codigo objeto no arquivo de saida*/
             corrige_endereco(modA, out, TabUsoA, TabGlobalDef);
             corrige_endereco(modB, out, TabUsoB, TabGlobalDef);
 
@@ -78,26 +60,15 @@ int main(int argc, char** argv){
         printf("ERRO: argumento(s) invalido(s).");
     }
 
-    // printf("\nTab uso MODA\n");
-    // print_tab_lst(TabUsoA);
-    // printf("\nTab def MODA\n");
-    // print_tab_lst(TabDefA);
-    // printf("\nTab uso MODB\n");
-    // print_tab_lst(TabUsoB);
-    // printf("\nTab def MODB\n");
-    // print_tab_lst(TabDefB);
-    // printf("\nTab global\n");
-    // print_tab_lst(TabGlobalDef);
-
     fclose(modA);
     fclose(modB);
 
     return 0;
 }
 
-void ler_objeto(FILE* modulo, Tab_lst **TabUso, Tab_lst **TabDef, Tab_lst **TabGlobalDef, int *fatCorAux, int i){
-  int dado;
+void ler_objeto(FILE* modulo, Tab_lst **TabUso, Tab_lst **TabDef, Tab_lst **TabGlobalDef, int *fatorCorrecao, int i){
 
+  int dado;
   char rotulo[10];
   char secao[10];
 
@@ -105,11 +76,11 @@ void ler_objeto(FILE* modulo, Tab_lst **TabUso, Tab_lst **TabDef, Tab_lst **TabG
   fscanf(modulo, "%s", secao);
   if(strcmp(secao, "TABLE") == 0){
 
-      /*TABLE USE*/
+      /*SECAO TABLE USE*/
       fscanf(modulo, "%s", secao);
       while(strcmp(secao, "TABLE") != 0){
         fscanf(modulo, "%s", secao);
-           /*salva dado na tabela de uso*/
+           /*salva simbolos e valores na tabela de uso*/
            if(strcmp(secao, "TABLE") != 0){
               strcpy(rotulo, secao);
               fscanf(modulo, "%d", &dado);
@@ -119,30 +90,30 @@ void ler_objeto(FILE* modulo, Tab_lst **TabUso, Tab_lst **TabDef, Tab_lst **TabG
 
       fscanf(modulo, "%s", secao);
 
-      /*TABLE DEFINITION*/
+      /*SECAO TABLE DEFINITION*/
       while(strcmp(secao, "RELATIVE") != 0){
-           /*salva dado da tabela de uso*/
+           /*salva simbolos e enderecos na tabela de definicoes*/
            fscanf(modulo, "%s", secao);
            if(strcmp(secao, "RELATIVE") != 0){
                 strcpy(rotulo, secao);
                 fscanf(modulo, "%d", &dado);
                 *TabDef = insert_tab_lst(*TabDef, rotulo, dado);
-                *TabGlobalDef = insert_tab_lst(*TabGlobalDef, rotulo, (dado+(*fatCorAux)));
+                *TabGlobalDef = insert_tab_lst(*TabGlobalDef, rotulo, (dado+(*fatorCorrecao)));
            }
       }
 
-      /*RELATIVE*/
+      /*SECAO RELATIVE*/
       while(strcmp(secao, "CODE") != 0){
-          /*salva dado dos enderecos relativos*/
+          /*Percorre secao dos enderecos relativos*/
           fscanf(modulo, "%s ", secao);
-          if(strcmp(secao, "CODE") != 0){
-              dado = atoi(secao);
-          }
+          // if(strcmp(secao, "CODE") != 0){
+          //     dado = atoi(secao);
+          // }
       }
 
-      /*CODE*/
+      /*SECAO CODE*/
       while(fscanf(modulo, "%d", &dado) != EOF){
-          *fatCorAux+=1;
+          *fatorCorrecao+=1;
       }
 
     }
@@ -151,27 +122,30 @@ return;
 
 void corrige_endereco(FILE* modulo, FILE* out, Tab_lst* TabUso, Tab_lst* TabGlobalDef){
 
-    int end = 0;
+    int end = 0; /*Inicializado como zero, pois indica o inicio do codigo obj*/
     int dado;
     char secao[10];
     Tab_lst* TabUsoAux = TabUso;
 
+    /*Percorre modulo ate secao de CODE(codigo objeto)*/
     fscanf(modulo, "%s", secao);
     while(strcmp(secao, "CODE") != 0){
       fscanf(modulo, "%s", secao);
-      //printf("%s", secao);
     }
-    //printf("%s", secao);
+    /*Faz a leitura do codigo objeto*/
     while(fscanf(modulo, "%d", &dado) != EOF){
-        //printf("\ndado : %d\n", dado);
+        /*Verifica tabela de uso*/
         if(TabUsoAux != NULL){
+            /*Verifica se o endereco do codigo obj esta na tabela de uso*/
             if(TabUsoAux->valor == end){
-              dado += procura_tab_lst(TabGlobalDef, TabUsoAux->simb);
-              TabUsoAux = TabUsoAux->next;
+                /*Caso esteja este eh atualizado de acordo com o
+                  valor presente na tabela global de definicoes*/
+                dado += procura_tab_lst(TabGlobalDef, TabUsoAux->simb);
+                TabUsoAux = TabUsoAux->next;
             }
         }
         end++;
-        /*salva dado da secao code*/
+        /*salva valor no arquivo de saida, que eh o codigo objeto ligado*/
         fprintf(out, "%d ", dado);
     }
 
